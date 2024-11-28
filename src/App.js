9e5e373dfc759453aaf0389c8ea4f356b24bdcba
vinctuan02 <vinctuan02@gmail.com>
2024-11-29 00:30:52 +0700

    header

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.scss';
 import Reports from './pages/Reports/Reports';
 import OtherPage from './pages/Others/OthersPage';
 import Sidebar from './components/Sidebar/Sidebar';
+import Header from './components/Header/HeaderComponent';
 
 const App = () => {
   const [isSidebarCollapsed, setIsSidebarCollapsed] = useState(false);
@@ -19,11 +20,19 @@ const App = () => {
         <div className={`sidebar-div ${isSidebarCollapsed ? 'collapsed' : 'expanded'}`}>
           <Sidebar toggleSidebar={toggleSidebar} isSidebarCollapsed={isSidebarCollapsed} />
         </div>
-        <div className="app__content">
-          <Routes>
-            <Route path="/reports" element={<Reports />} />
-            <Route path="/others-page" element={<OtherPage />} />
-          </Routes>
+        <div className={`app__main ${isSidebarCollapsed ? 'collapsed' : 'expanded'}`}>
+          <div className='header'>
+            <Header />
+          </div>
+          <div className="app__content">
+            <Routes>
+              <Route path="/reports" element={<Reports />} />
+              <Route path="/page2" element={<page2 />} />
+              <Route path="/page3" element={<page3 />} />
+              <Route path="/page4" element={<page4 />} />
+              <Route path="/others-page" element={<OtherPage />} />
+            </Routes>
+          </div>
         </div>
       </div>
     </Router>
---
+++ src/components/Header/HeaderComponent.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import './HeaderComponent.scss'
+import { Button } from '@mui/material'
+import { GridSearchIcon } from '@mui/x-data-grid'
+
+const Header = () => {
+    return (
+        <div className='container-header-component'>
+            <div className='header-left'>
+                <Button className='button-custom'>Notes <i className='bx bxs-chevron-down'></i></Button>
+                <Button className='button-custom'>QUERY SQL <i className='bx bxs-chevron-down'></i></Button>
+            </div>
+            <div className='header-right'>
+                <div className='search-container'>
+                    <GridSearchIcon className='search-icon' />
+                    <input className='search' type="text" placeholder="Search..." />
+                </div>
+                <div style={{ display: "flex", alignItems: 'center' }}>
+                    {/* <div>
+                    <Button>Option <i className='bx bxs-chevron-down'></i></Button>
+                </div>
+                <div>
+                    <Button>Option <i className='bx bxs-chevron-down'></i></Button>
+                </div>
+                <div>
+                    <Button>Option <i className='bx bxs-chevron-down'></i></Button>
+                </div> */}
+                    {/* <div>
+                    <Button>Option <i className='bx bxs-chevron-down'></i></Button>
+                </div> */}
+                    <div className='container-header-icon'>
+                        <i className='bx bx-bell'></i>
+                        <i className='bx bx-user-circle'></i>
+                        <i className="bx bx-menu"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
